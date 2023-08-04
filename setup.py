af7169e1b02c09315d166699f9d3c2565fc21d9c
maamari <maamari@usc.edu>
2023-08-03 20:21:43 -0700

    update setup

--- setup.py
+++ setup.py
@@ -16,9 +16,13 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     classifiers=[
-        "Programming Language :: Python :: 3.6+",
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
         "License :: OSI Approved :: MIT License",
-        "Operating System :: POSIX :: Linux",
     ],
     python_requires='>=3.6',
     install_requires=[
